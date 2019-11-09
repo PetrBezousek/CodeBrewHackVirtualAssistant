1bfba0cba1846662011f3f7b168f463a1e66daf1
Filip Burger <burgerfiip@gmail.com>
2019-11-09 17:14:12 +0100

    start version

--- get_messages.py
+++ get_messages.py
@@ -1,8 +1,25 @@
 import requests
-from config import token
+from datetime import datetime, time, timezone
+import call_api
 
-endpoint = 'https://slack.com/api/channels.history'
-# channels:history
 
-response = requests.get('https://slack.com/api/channels.history?token={0}&channel=CQC5H8W6R&pretty=1'.format(token))
-print(response.text)
+def get_midnight_timestamp():
+    midnight = datetime.combine(datetime.now().date(), time(0,0))
+    timestamp = midnight.replace(tzinfo=timezone.utc).timestamp()
+    return midnight.timestamp()
+
+
+def get_messages(channel='CQC5H8W6R',user=None):
+
+    params = []
+    scope = 'channels.history'
+    oldest = str(get_midnight_timestamp())
+    params.append('channel='+channel,'oldest='+oldest,'pretty=1')
+
+    if user:
+        params.append('user='+user)
+
+    return call_api('GET',scope, params)
+
+
+print(get_messages())

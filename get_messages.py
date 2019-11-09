14322e31fc99f4e46872c5409530420cc5225745
Petr Bezoušek <petr.bezza.bezousek@gmail.com>
2019-11-09 16:57:51 +0100

    Better ignore

--- get_messages.py
+++ get_messages.py
@@ -1,8 +1,8 @@
 import requests
-from config import token
+from config import token_god
 
 endpoint = 'https://slack.com/api/channels.history'
 # channels:history
 
-response = requests.get('https://slack.com/api/channels.history?token={0}&channel=CQC5H8W6R&pretty=1'.format(token))
+response = requests.get('https://slack.com/api/channels.history?token={0}&channel=CQC5H8W6R&pretty=1'.format(token_bot))
 print(response.text)

46703722bac55bed76913156d18d25874caddf88
Petr Bezoušek <petr.bezza.bezousek@gmail.com>
2019-11-09 16:37:50 +0100

    Fix GET request, return

--- call_api.py
+++ call_api.py
@@ -13,11 +13,13 @@ def call_slack_api(method, scope, params=[]):
                                                            scope=scope,
                                                            params=params))
     if method == "GET":
-        resp = requests.post('{url}{scope}{params}'.format(url=api_url,
-                                                           scope=scope,
-                                                           params=params))                                                       
+        resp = requests.get('{url}{scope}{params}'.format(url=api_url,
+                                                          scope=scope,
+                                                          params=params))
+                                                            
     print(resp.text)
+    return resp
 
 # TEST volani
-# params = ['channel=CQC5H8W6R', 'text=HelloWorld!', 'token={token}'.format(token=token)]
-# call_slack_api("POST", "chat.postMessage", params)
+# params = ['channel=CQC5H8W6R', 'text=HelloWorld!', 'token={token}'.format(token=token), 'pretty=1']
+# call_slack_api("GET", "channels.history", params)

dc645ec2dfe5866be0ff241b794f9b72e0be20f6
Petr Bezoušek <petr.bezza.bezousek@gmail.com>
2019-11-09 15:01:23 +0100

    Config with token

--- get_messages.py
+++ get_messages.py
@@ -1,6 +1,6 @@
 import requests
+from config import token
 
-token = 'xoxp-830034645303-827714821620-816714109955-055b8d24972aa72fa9a3f735f1fd10b6'
 endpoint = 'https://slack.com/api/channels.history'
 # channels:history
 

faf52a41c3245389dec89e0476cf2adf8ad62320
Filip Burger <burgerfiip@gmail.com>
2019-11-09 14:54:53 +0100

    start version

--- get_messages.py
+++ get_messages.py
@@ -1,6 +1,6 @@
 import requests
 
-token = 'xoxp-830034645303-827714821620-816768327875-cdc25b44c301a3ae2a00abb691c6da92'
+token = 'xoxp-830034645303-827714821620-830112343751-d6683c335cdabe0d0779a90fc9599f32'
 endpoint = 'https://slack.com/api/channels.history'
 # channels:history
 

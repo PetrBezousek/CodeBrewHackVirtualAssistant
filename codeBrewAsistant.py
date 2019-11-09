d2888300de540d95c205fa6a38e03be7145d1657
miro <mgr.miroslav.murar@gmail.com>
2019-11-09 18:43:08 +0100

    UpdatepredPoolom_NeviemCoJeNove

--- codeBrewAsistant.py
+++ codeBrewAsistant.py
@@ -5,14 +5,12 @@ def answerHello(inpt, user):
 
 def whereIs(inpt, location):
     strip_codeBrewAsistant = inpt.strip("@codeBrewAsistant")
-    finaloutput = strip_codeBrewAsistant.strip(" where is ")
+    finaloutput = strip_codeBrewAsistant.strip(" where is    ")
     return f'{finaloutput} is in {location}'
 
 def whenIsFree(inpt, time):
-    # print(inpt)
     strip_codeBrewAsistant = inpt.strip("@codeBrewAsistant")
     finaloutput = strip_codeBrewAsistant.strip(" when is free ")
-
     return f'{finaloutput} is free at {time}'
 
 print(answerHello('@codeBrewAsistant ', 'Miro'))
